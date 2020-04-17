a559e910058bbcdea1e1064f83f3a49e664c184d
Naomi Ruemmely Torres <59476335+NaomiPriest97@users.noreply.github.com>
2020-04-16 22:32:35 -0400

    saving

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,20 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './data/recipes.db3'
     },
-    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
   },
 
